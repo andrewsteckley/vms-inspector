1002879fafdd2eb19781c433976b9bc5f1693d2f
andrew <andrew@steckleyandassociates.com>
2023-03-05 11:18:37 -0700

    update of VMS Inspector

---
+++ go.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+cp ../Voynich/VMS_Inspector/hires/02*.jpg hires/.
+git add hires/02*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/03*.jpg hires/.
+git add hires/03*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/04*.jpg hires/.
+git add hires/04*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/05*.jpg hires/.
+git add hires/05*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/06*.jpg hires/.
+git add hires/06*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/07*.jpg hires/.
+git add hires/07*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/08*.jpg hires/.
+git add hires/08*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/09*.jpg hires/.
+git add hires/09*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/10*.jpg hires/.
+git add hires/10*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/11*.jpg hires/.
+git add hires/11*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/12*.jpg hires/.
+git add hires/12*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/13*.jpg hires/.
+git add hires/13*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/14*.jpg hires/.
+git add hires/14*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/15*.jpg hires/.
+git add hires/15*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/16*.jpg hires/.
+git add hires/16*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/17*.jpg hires/.
+git add hires/17*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/18*.jpg hires/.
+git add hires/18*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/19*.jpg hires/.
+git add hires/19*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/20*.jpg hires/.
+git add hires/20*.jpg
+git commit -m more
+git push
+
+cp ../Voynich/VMS_Inspector/hires/21*.jpg hires/.
+git add hires/21*.jpg
+git commit -m more
+git push
--- vms_inspector.js
+++ vms_inspector.js
@@ -1,86 +1,96 @@
-ww = 2894
-hh = 3797
+ww = 2894 // width of hires images
+hh = 3797 // height of hires images
 image_aspect = hh/ww
 lenses_on = true
 
-function imageZoom(imgID, result_1_ID, result_2_ID, result_3_ID) {
-  var img, lens, result, cx, cy;
-  img = document.getElementById(imgID);
-  result_1 = document.getElementById(result_1_ID);
-  result_2 = document.getElementById(result_2_ID);
-  result_3 = document.getElementById(result_3_ID);
+
+function imageZoom(src_img_ID, magnified_img_1_ID, magnified_img_2_ID) {
+  var src_img, lens, result, cx, cy;
+  src_img = document.getElementById(src_img_ID);
+  mag_img_1 = document.getElementById(magnified_img_1_ID);
+  mag_img_2 = document.getElementById(magnified_img_2_ID);
 
   /* keep only the specified width */
-  img.offsetHeight = img.offsetWidth * image_aspect;
-<!--  img.height = img.width * image_aspect;-->
+//  src_img.offsetHeight = src_img.offsetWidth * image_aspect;
+<!--  src_img.height = src_img.width * image_aspect;-->
 
   /*create lens: */
   lens = document.createElement("DIV");
   lens.setAttribute("class", "img-zoom-lens");
 
-  /*create lens_wide: */
-  lens_wide = document.createElement("DIV");
-  lens_wide.setAttribute("class", "img-zoom-lens-wide");
+  /*create wide_lens: */
+  wide_lens = document.createElement("DIV");
+  wide_lens.setAttribute("class", "img-zoom-lens-wide");
+
+
+  image_aspect = src_img.height / src_img.width
+//  mag_img_1.width = src_img.width
+//  mag_img_1.height = src_img.height
+  mag_img_2.width = 2*src_img.width
+  mag_img_2.height = src_img.height
+
+    console.log("img src="+src_img.src)
+    console.log("src_img.width="+src_img.width)
+    console.log("src_img.naturalWidth="+src_img.naturalWidth)
+    console.log("src_img.naturalHeight="+src_img.naturalHeight)
+    console.log("src_img.clientWidth="+src_img.clientWidth)
+    console.log("src_img.clientHeight="+src_img.clientHeight)
+
 
 
   /* set other sizes and positions */
-  lens_width = (0.1 * img.width)
-  lens_height = (0.1 * img.height)
-//  lens_size = (0.1 * img.height)
+  lens_width = Number(0.1 * src_img.width)
+  lens_height = Number(0.1 * src_img.height)
   lens.style.width = lens_width + 'px';
   lens.style.height = lens_height + 'px';
 
-  lens_wide_width = img.width
-  lens_wide_height = (0.05 * img.height)
-  lens_wide.style.width = img.width + 'px';
-  lens_wide.style.height = lens_wide_height + 'px';
-
-  result_1.style.width = img.offsetWidth;
-  result_1.style.height = img.offsetHeight;
-  result_1.offsetWidth = img.offsetWidth;
-  result_1.offsetHeight = img.offsetHeight;
-  result_2.offsetWidth = 2*img.offsetWidth;
-  result_2.offsetHeight = 2*img.offsetHeight;
-
-  result_3.style.width = img.offsetWidth;
-  result_3.style.height = img.offsetHeight;
-  result_3.offsetWidth = img.offsetWidth;
-  result_3.offsetHeight = img.offsetHeight;
+  wide_lens_width = src_img.width
+  wide_lens_height = Number(0.05 * src_img.height)
+  wide_lens.style.width = wide_lens_width + 'px';
+  wide_lens.style.height = wide_lens_height + 'px';
 
   /*insert lens:*/
-  img.parentElement.insertBefore(lens, img);
-  img.parentElement.insertBefore(lens_wide, img);
+  src_img.parentElement.insertBefore(lens, src_img);
+  src_img.parentElement.insertBefore(wide_lens, src_img);
 
-  /*calculate the ratio between result DIVs and lens:*/
-//  hor_offset = ((img.offsetWidth - img.width)/2)
-//  hor_offset = ((img.offsetWidth - img.width)/2)+1500
-//  ver_offset = ((img.offsetHeight - img.height)/2)+1500
-  cx_1 = result_1.offsetWidth / lens.offsetWidth;
-  cy_1 = result_1.offsetHeight / lens.offsetHeight;
-  cx_2 = result_2.offsetWidth / lens_wide.offsetWidth;
-  cy_2 = result_2.offsetHeight / lens_wide.offsetHeight;
+  mag_1_magnification_x = mag_img_1.offsetWidth / lens.offsetWidth;
+  mag_1_magnification_y = mag_img_1.offsetHeight / lens.offsetHeight;
+
+  mag_2_magnification_x = mag_img_2.offsetWidth / wide_lens.offsetWidth;
+  mag_2_magnification_y = mag_img_2.offsetHeight / wide_lens.offsetHeight;
 
 
   /*set background properties for the result DIV:*/
-  result_1.style.backgroundImage = "url('" + img.src + "')";
-  result_1.style.backgroundSize = (img.width * cx_1) + "px " + (img.height * cy_1) + "px";
-  result_2.style.backgroundImage = "url('" + img.src + "')";
-  result_2.style.backgroundSize = (img.width *  cx_2) + "px " + (img.height * cy_2) + "px";
-  result_3.style.backgroundImage = "url('" + img.src + "')";
-  result_3.style.backgroundSize = (img.width * cx_1) + "px " + (img.height * cy_1) + "px";
+  mag_img_1.style.backgroundImage = "url('" + src_img.src + "')";
+  mag_img_1.style.backgroundSize = (src_img.width * mag_1_magnification_x) + "px " + (src_img.height * mag_1_magnification_y) + "px";
+
+  mag_img_2.style.backgroundImage = "url('" + src_img.src + "')";
+  mag_img_2.style.backgroundSize = (src_img.width * mag_2_magnification_x) + "px " + (src_img.height * mag_2_magnification_y) + "px";
+//  alert("mag_img_2.style.backgroundSize="+mag_img_2.style.backgroundSize)
 
   /*execute a function when someone moves the cursor over the image, or the lens:*/
+
+  src_img.addEventListener("mousemove", moveLenses);
+//  src_img.addEventListener("mousemove", moveWideLense);
+  src_img.addEventListener("click", toggleLenses);
+
   lens.addEventListener("mousemove", moveLenses);
-  lens_wide.addEventListener("mousemove", moveLenses);
-  img.addEventListener("mousemove", moveLenses);
-  lens_wide.addEventListener("click", toggleLenses);
-  img.addEventListener("click", toggleLenses);
+  lens.addEventListener("click", toggleLenses);
+
+  wide_lens.addEventListener("mousemove", moveLenses);
+  wide_lens.addEventListener("click", toggleLenses);
+
 
   /*and also for touch screens:*/
-  lens.addEventListener("touchmove", moveLenses);
-  img.addEventListener("touchmove", moveLenses);
-  lens_wide.addEventListener("touchmove", moveLenses);
-//  img.addEventListener("touchclick", toggleLenses);
+//  lens.addEventListener("touchmove", moveLense);
+//  src_img.addEventListener("touchmove", moveLense);
+//  wide_lens.addEventListener("touchmove", moveWideLense);
+//  src_img.addEventListener("touchclick", toggleLenses);
+
+    lens_half_width = Number(lens.offsetWidth / 2)
+    lens_half_height = Number(lens.offsetHeight / 2)
+    wide_lens_half_width = Number(wide_lens.offsetWidth / 2)
+    wide_lens_half_height = Number(wide_lens.offsetHeight / 2)
 
 
   function toggleLenses(e) {
@@ -88,239 +98,120 @@ function imageZoom(imgID, result_1_ID, result_2_ID, result_3_ID) {
 //    print("toggling")
   }
 
-function moveLenses(e) {
+  function moveLenses(e) {
     if (!lenses_on ) {
         return;
     }
-//    print(lenses_on)
     var pos, x, y;
     /*prevent any other actions that may occur when moving over the image:*/
     e.preventDefault();
+
     /*get the cursor's x and y positions:*/
     pos = getCursorPos(e);
+
     /*calculate the position of the lens:*/
-    x = pos.x - (lens.offsetWidth / 2);
-    y = pos.y - (lens.offsetHeight / 2);
+    mag_1_rect_left = pos.x_into_img - lens_half_width;
+    mag_1_rect_top  = pos.y_into_img - lens_half_height;
+
+    mag_2_rect_left = pos.x_into_img - wide_lens_half_width;
+    mag_2_rect_top  = pos.y_into_img - wide_lens_half_height;
+
     /*prevent the lens from being positioned outside the image:*/
-    if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
-    if (x < 0) {x = 0;}
-    if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
-    if (y < 0) {y = 0;}
-//    y2 = y;
-    y2 = y + (lens.offsetHeight / 4 );
-    /*set the position of the lens:*/
-    lens.style.left = x + "px";
-    lens.style.top = y + "px";
-    lens_wide.style.top = y2 + "px";
+    if (mag_1_rect_left > src_img.width - lens.offsetWidth)  {mag_1_rect_left = src_img.width  - lens.offsetWidth;}
+    if (mag_1_rect_left < lens_half_width) {mag_1_rect_left = 0;}
+    if (mag_1_rect_top > (src_img.height - lens.offsetHeight)) {mag_1_rect_top  = src_img.height - lens.offsetHeight;}
+    if (mag_1_rect_top < 0 ) {mag_1_rect_top = 0;}
+
+    if (mag_2_rect_left > src_img.width - wide_lens.offsetWidth) {mag_2_rect_left = src_img.width - wide_lens.offsetWidth;}
+    if (mag_2_rect_left < wide_lens_half_width) {mag_2_rect_left = 0;}
+    if (mag_2_rect_top > src_img.height - wide_lens.offsetHeight) {mag_2_rect_top = src_img.height - wide_lens.offsetHeight;}
+    if (mag_2_rect_top < 0) {mag_2_rect_top = 0;}
+
+    lens_left = pos.src_img_client_rec_left  + mag_1_rect_left
+    lens_top = mag_1_rect_top + pos.mag_img_2_client_rec_height + pos.unidentified_vertical_offset
+
+    wide_lens_left = pos.src_img_client_rec_left  + mag_2_rect_left
+    wide_lens_top =  mag_2_rect_top + pos.mag_img_2_client_rec_height + pos.unidentified_vertical_offset
+
+    lens.style.left = lens_left + "px";
+    lens.style.top = lens_top + "px";
+
+    wide_lens.style.left = wide_lens_left + "px";
+    wide_lens.style.top = wide_lens_top + "px";
+
     /*display what the lens "sees":*/
-    result_1.style.backgroundPosition = "-" + (x * cx_1) + "px -" + (y * cy_1) + "px";
-<!--    result_2.style.backgroundPosition = "-" + (x * cx_2) + "px -" + (y * cy_2) + "px";-->
-    result_2.style.backgroundPosition = "-" + (0) + "px -" + (y2 * cy_2) + "px";
+    mag_img_1.style.backgroundPosition =
+                                        "-" + (mag_1_rect_left * mag_1_magnification_x) + "px " +
+                                        "-" + (mag_1_rect_top * mag_1_magnification_y) + "px";
+    mag_img_2.style.backgroundPosition = "-" + (0) + "px " +
+                                         "-" + (mag_2_rect_top * mag_2_magnification_y) + "px";
 }
 
   function getCursorPos(e) {
     var a, x = 0, y = 0;
     e = e || window.event;
     /*get the x and y positions of the image:*/
-    a = img.getBoundingClientRect();
+    src_img_client_rec = src_img.getBoundingClientRect();
+    mag_img_2_client_rec = mag_img_2.getBoundingClientRect();
+    unidentified_vertical_offset = 13;
+
     /*calculate the cursor's x and y coordinates, relative to the image:*/
-    x = e.pageX - a.left;
-    y = e.pageY - a.top;
+    x_into_img = e.pageX - src_img_client_rec.left;
+    y_into_img = e.pageY - src_img_client_rec.top;
+
     /*consider any page scrolling:*/
-    x = x - window.pageXOffset;
-    y = y - window.pageYOffset;
-    return {x : x, y : y};
-  }
-//  alert("redy")
-//  capture_element = document.getElementById(capture_ID);
-//  capture_element.addEventListener("click", oncapture);
-//  crop_setup()
+    x_into_img = x_into_img - window.pageXOffset;
+    y_into_img = y_into_img - window.pageYOffset;
 
-
-//        $('#capture').click(function(){
-//        alert("redy")
-//       entry = $('#message').html()
-//
-//        tx = $('#x').val();
-//        ty = $('#y').val();
-//        tx2 = $('#x2').val();
-//        ty2 = $('#y2').val();
-//        tw = $('#w').val();
-//        th = $('#h').val();
-//        entry = entry + tx  + ' ' + ty + ' ' + tx2 + ' ' + ty2 + ' ' + tw +' ' + th + "<br>"
-////        alert( tx  + ' ' + ty + ' ' + tx2 + ' ' + ty2 + ' ' + tw +' ' + th)
-////        alert(t);
-//
-//        //The new text that we want to show.
-//        var newText = 'This text has been changed!';
-//
-//        //Change the text using the text method.
-//        $('#message').html(entry);
-//
-//    });
-
-
-
-}
-
-
-function magnify(imgID, zoom) {
-  var img, glass, w, h, bw;
-  img = document.getElementById(imgID);
-
-  /* Create magnifier glass: */
-  glass = document.createElement("DIV");
-  glass.setAttribute("class", "img-magnifier-glass");
-
-  /* Insert magnifier glass: */
-  img.parentElement.insertBefore(glass, img);
-
-  /* Set background properties for the magnifier glass: */
-  glass.style.backgroundImage = "url('" + img.src + "')";
-  glass.style.backgroundRepeat = "no-repeat";
-  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
-  bw = 3;
-  w = glass.offsetWidth / 2;
-  h = glass.offsetHeight / 2;
-
-  /* Execute a function when someone moves the magnifier glass over the image: */
-  glass.addEventListener("mousemove", moveMagnifier);
-  img.addEventListener("mousemove", moveMagnifier);
-
-  /*and also for touch screens:*/
-  glass.addEventListener("touchmove", moveMagnifier);
-  img.addEventListener("touchmove", moveMagnifier);
-  function moveMagnifier(e) {
-    var pos, x, y;
-    /* Prevent any other actions that may occur when moving over the image */
-    e.preventDefault();
-    /* Get the cursor's x and y positions: */
-    pos = getCursorPos(e);
-    x = pos.x;
-    y = pos.y;
-    /* Prevent the magnifier glass from being positioned outside the image: */
-    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
-    if (x < w / zoom) {x = w / zoom;}
-    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
-    if (y < h / zoom) {y = h / zoom;}
-    /* Set the position of the magnifier glass: */
-    glass.style.left = (x - w) + "px";
-    glass.style.top = (y - h) + "px";
-    /* Display what the magnifier glass "sees": */
-    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
-  }
-
-  function getCursorPos(e) {
-    var a, x = 0, y = 0;
-    e = e || window.event;
-    /* Get the x and y positions of the image: */
-    a = img.getBoundingClientRect();
-    /* Calculate the cursor's x and y coordinates, relative to the image: */
-    x = e.pageX - a.left;
-    y = e.pageY - a.top;
-    /* Consider any page scrolling: */
-    x = x - window.pageXOffset;
-    y = y - window.pageYOffset;
-    return {x : x, y : y};
+    return {x_into_img : x_into_img,
+    y_into_img : y_into_img,
+    pageX: e.pageX,
+    pageY: e.pageY,
+    src_img_client_rec_left: src_img_client_rec.left,
+    src_img_client_rec_top:src_img_client_rec.top,
+    mag_img_2_client_rec_height: mag_img_2_client_rec.height,
+    mag_img_2_client_rec_top:mag_img_2_client_rec.top,
+    unidentified_vertical_offset: unidentified_vertical_offset};
   }
 }
 
 
+function ToggleDisplayHideRows(checkbox){
+    row_id_to_toggle = `src_${checkbox.id}`
+    rows_to_toggle  = document.querySelectorAll(`.${row_id_to_toggle}`);
+    len = rows_to_toggle.length
+    if (checkbox.checked) {
+        for (let i = 0; i < len; i++) {
+            row_to_toggle = rows_to_toggle[i]
+            row_to_toggle.style.display = 'table-row';
+        }
+    }else{
+        for (let i = 0; i < len; i++) {
+            row_to_toggle = rows_to_toggle[i]
+		    row_to_toggle.style.display = 'none';
+        }
+    }
+}
 
-function crop_setup(){
-    // for sample 1
-    alert("crop setup")
-    $('#myresult3').Jcrop({ // we linking Jcrop to our image with id=cropbox1
-        aspectRatio: 0,
-        onChange: updateCoords,
-        onSelect: updateCoords
-    });
+
+function prevImg(){
+  document.getElementById("previous").src = "left_icon.png";
+};
+function prevHoverImg(){
+  document.getElementById("previous").src = "left_hover_icon.png";
 };
 
-function updateCoords(c) {
-    alert("updateing")
-    $('#x').val(c.x);
-    $('#y').val(c.y);
-    $('#w').val(c.w);
-    $('#h').val(c.h);
-
-    $('#x2').val(c.x2);
-    $('#y2').val(c.y2);
-
-
-    var rx = 200 / c.w; // 200 - preview box size
-    var ry = 200 / c.h;
-
-    $('#preview').css({
-        width: Math.round(rx * 800) + 'px',
-        height: Math.round(ry * 600) + 'px',
-        marginLeft: '-' + Math.round(rx * c.x) + 'px',
-        marginTop: '-' + Math.round(ry * c.y) + 'px'
-    });
+function nextImg(){
+  document.getElementById("next").src = "right_icon.png";
+};
+function nextHoverImg(){
+  document.getElementById("next").src = "right_hover_icon.png";
 };
 
-//function oncapture(e){
-////    alert("oncpatuer")
-//       entry = $('#message').html()
-//
-//        tx = $('#x').val();
-//        ty = $('#y').val();
-//        tx2 = $('#x2').val();
-//        ty2 = $('#y2').val();
-//        tw = $('#w').val();
-//        th = $('#h').val();
-//        entry = entry + tx  + ' ' + ty + ' ' + tx2 + ' ' + ty2 + ' ' + tw +' ' + th + "<br>"
-//        $('#message').html(entry);
-//}
-
-
-//setup() {
-//
-//        $('#capture').click(function(){
-//        alert("redy")
-//       entry = $('#message').html()
-//
-//        tx = $('#x').val();
-//        ty = $('#y').val();
-//        tx2 = $('#x2').val();
-//        ty2 = $('#y2').val();
-//        tw = $('#w').val();
-//        th = $('#h').val();
-//        entry = entry + tx  + ' ' + ty + ' ' + tx2 + ' ' + ty2 + ' ' + tw +' ' + th + "<br>"
-////        alert( tx  + ' ' + ty + ' ' + tx2 + ' ' + ty2 + ' ' + tw +' ' + th)
-////        alert(t);
-//
-//        //The new text that we want to show.
-//        var newText = 'This text has been changed!';
-//
-//        //Change the text using the text method.
-//        $('#message').html(entry);
-//
-//    });
-//   };
-
- $(document).ready(function () {
-
-        $('#capture').click(function(){
-       entry = $('#message').html()
-
-        tx = $('#x').val();
-        ty = $('#y').val();
-        tx2 = $('#x2').val();
-        ty2 = $('#y2').val();
-        tw = $('#w').val();
-        th = $('#h').val();
-        entry = entry + tx  + ' ' + ty + ' ' + tx2 + ' ' + ty2 + ' ' + tw +' ' + th + "<br>"
-//        alert( tx  + ' ' + ty + ' ' + tx2 + ' ' + ty2 + ' ' + tw +' ' + th)
-//        alert(t);
-
-        //The new text that we want to show.
-        var newText = 'This text has been changed!';
-
-        //Change the text using the text method.
-        $('#message').html(entry);
-
-    });
-        });
-
+function tocImg(){
+  document.getElementById("toc").src = "toc_icon.png";
+};
+function tocHoverImg(){
+  document.getElementById("toc").src = "toc_hover_icon.png";
+};
 
